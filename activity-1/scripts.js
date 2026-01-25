875aa478602114c3ebafb7696e15bb7288d36555
Greg Perez <perezga1@matc.edu>
2026-01-25 16:51:52 -0600

    Complete Activity 1

---
+++ activity-1/scripts.js
@@ -0,0 +1,29 @@
+console.log("Hello World");
+
+document.getElementById("output").innerHTML = "Hello, World!";
+
+let studentName = "Greg Perez";
+const age = 34;
+let isStudent = true;
+let emptyValue = null;
+let notAssigned;
+
+console.log("String variable studentName = Greg Perez");
+console.log("Constant number variable age = 34");
+console.log("Boolean variable isStudent = true");
+console.log("Null variable emptyValue is a null object");
+console.log("Undefined variable notAssigned is undefined");
+
+console.log(typeof studentName);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof emptyValue);
+console.log(typeof notAssigned);
+
+studentName = "John Doe";
+
+console.log("String variable studentName updated to " + studentName);
+
+age = 35;
+
+
